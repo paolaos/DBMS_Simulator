d567f5a59e768ce57826f232eacbe145c2f79c03
Brayan <bjgd95@gmail.com>
2017-02-03 15:09:55 -0600

    Bla bla

--- src/Bla.java
+++ src/Bla.java
@@ -2,5 +2,6 @@ public class Bla {
     public static void main(String... args){
         System.out.println("Some shit :O");
         System.out.println("More shit :D");
+        System.out.println("Some more shit :O");
     }
 }
