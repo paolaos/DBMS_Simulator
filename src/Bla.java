b335f30e2d4879345b7834aebdf47e6ffb7430e6
Brayan <bjgd95@gmail.com>
2017-02-03 23:01:06 -0600

    Distibuciones parametrizadas

--- src/Bla.java
+++ src/Bla.java
@@ -1,7 +1,16 @@
 public class Bla {
+
+
     public static void main(String... args){
-        System.out.println("Some shit :O");
-        System.out.println("More shit :D");
-        System.out.println("Some more shit :O");
+        DistributionGenerator DG = new DistributionGenerator();
+
+        int x= Math.round((float) 0.1);
+        int y = (int) 0.1;
+        for(int i=0; i<20; i++) {
+           // System.out.println(x + "  " + y + "   " + DG.getNextRandomValueByuniform(1, 64));
+            DistributionGenerator.Type p= DistributionGenerator.Type.DDL;
+            System.out.println( DG.timeInQueryProcessingModule(p));
+        }
+
     }
 }
---
+++ src/DistributionGenerator.java
@@ -0,0 +1,108 @@
+import java.util.Random;
+
+/**
+ * Created by jesus on 03/02/2017.
+ */
+
+
+public class DistributionGenerator {
+
+    public enum Type{
+        SELECT, UPDATE, JOIN, DDL
+    }
+
+    Random rnd;
+
+
+
+        public DistributionGenerator(){
+            rnd = new Random();
+
+        }
+
+        public Type  generetaType(){
+
+            float randomNumber= rnd.nextFloat();
+            Type query;
+
+            if(randomNumber<0.32){
+                query= Type.SELECT;
+            }else if(randomNumber>0.31 && randomNumber <0.60){
+                query = Type.UPDATE;
+            }else if (randomNumber > 0.59 && randomNumber <0.93 ){
+                query = Type.JOIN;
+            }else{
+                query =Type.DDL;
+            }
+
+            return  query;
+        }
+
+
+         public float getNextArrival(float lambda){
+            float aleatoryNumber = this.rnd.nextFloat();
+            return  (float) -Math.log(aleatoryNumber)/lambda;
+         }
+
+         public float getNextRandomValueByuniform(float a, float b){
+             float r = this.rnd.nextFloat();
+             return (float)  (r*(b-a))+ a;
+         }
+
+         public  float getNextRandomValueByExponential( float lambda ){
+             float r = this.rnd.nextFloat();
+
+             return  (float) (-1/(lambda*Math.log(r)));
+
+         }
+
+         public float getNextRandomValueByNormal( float average, float estandarDeviation){
+            float z=0;
+            float x;
+            for (int i =0; i <12; i++){
+                z+=this.rnd.nextFloat();
+            }
+            z = z -6;
+            x = average + estandarDeviation* z;
+            return  x;
+         }
+
+
+
+         public float timeInQueryProcessingModule(Type query){
+             float totalTime=0;
+             float lexicalValidationTime ;
+             float syntacticalValidationTime;
+             float semanticValidationTime;
+             float permitVerificationTime;
+             float queryOptimizationTime;
+             float aleatoryNumber=this.rnd.nextFloat();
+
+             if(aleatoryNumber <0.7){
+                 lexicalValidationTime =(float)0.1;
+             }else {
+                 lexicalValidationTime=(float) 0.4;
+             }
+
+              syntacticalValidationTime = this.getNextRandomValueByuniform(0,(float) 0.8);
+              semanticValidationTime    = this.getNextRandomValueByNormal(1, (float) 0.5 );
+              permitVerificationTime    = this.getNextRandomValueByExponential((float) (1/0.7));
+
+              if(query.equals(Type.SELECT)|| query.equals(Type.JOIN)){
+                  queryOptimizationTime =(float)0.1;
+              }else{
+                  queryOptimizationTime =(float) 0.5;
+              }
+             System.out.println("lexicalValidationTime "+ lexicalValidationTime);
+             System.out.println("syntacticalValidationTime "+  syntacticalValidationTime);
+             System.out.println("semanticValidationTime "+ semanticValidationTime);
+             System.out.println("permitVerificationTime "+ permitVerificationTime);
+             System.out.println("queryOptimizationTime "+ queryOptimizationTime+"\n");
+             totalTime=lexicalValidationTime + syntacticalValidationTime + semanticValidationTime + permitVerificationTime +queryOptimizationTime;
+             return totalTime;
+         }
+
+
+
+
+}
