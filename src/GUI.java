7e4c5b6c805f2f79d1c796a60b6a54a8ebed8f54
Paola Ortega S <paola@saborio.org>
2017-02-13 17:54:08 -0600

    Mejoras a la interfaz

--- src/GUI.java
+++ src/GUI.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import javax.swing.border.Border;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
@@ -69,10 +70,10 @@ public class GUI extends JFrame{
     public GUI() {
         super.setTitle(TITLE);
         super.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        super.setLayout(new GridLayout(0, 1, 0, 20));
-
         lblNumberOfSimulations = new JLabel(NUMBER_OF_SIMULATIONS);
+        lblNumberOfSimulations.setFont(new Font("Normal", Font.BOLD, 18));
         txtNumberOfSimulations = new JTextField();
+        txtNumberOfSimulations.setFont(new Font("Normal", Font.BOLD, 18));
         txtNumberOfSimulations.setColumns(20);
         panelNumberOfSimulations = new JPanel();
         panelNumberOfSimulations.setLayout(new BorderLayout());
@@ -80,14 +81,17 @@ public class GUI extends JFrame{
         panelNumberOfSimulations.add(txtNumberOfSimulations, BorderLayout.EAST);
 
         lblMaxTimePerSimulation = new JLabel(MAX_TIME_PER_SIMULATION);
+        lblMaxTimePerSimulation.setFont(new Font("Normal", Font.BOLD, 18));
         txtMaxTimePerSimulation = new JTextField();
         txtMaxTimePerSimulation.setColumns(20);
+        txtMaxTimePerSimulation.setFont(new Font("Normal", Font.BOLD, 18));
         panelMaxTimePerSimulation = new JPanel();
         panelMaxTimePerSimulation.setLayout(new BorderLayout());
         panelMaxTimePerSimulation.add(lblMaxTimePerSimulation, BorderLayout.WEST);
         panelMaxTimePerSimulation.add(txtMaxTimePerSimulation, BorderLayout.EAST);
 
         lblSlowMode = new JLabel(SLOW_MODE);
+        lblSlowMode.setFont(new Font("Normal", Font.BOLD, 18));
         chkSlowMode = new JCheckBox();
         chkSlowMode.setSelected(false);
         chkSlowMode.addChangeListener(new ChangeListener() {
@@ -106,7 +110,9 @@ public class GUI extends JFrame{
         panelSlowMode.add(chkSlowMode, BorderLayout.EAST);
 
         lblDelay = new JLabel(DELAY);
+        lblDelay.setFont(new Font("Normal", Font.BOLD, 18));
         txtDelay = new JTextField();
+        txtDelay.setFont(new Font("Normal", Font.BOLD, 18));
         txtDelay.setColumns(20);
         txtDelay.setEnabled(false);
         panelDelay = new JPanel();
@@ -115,46 +121,57 @@ public class GUI extends JFrame{
         panelDelay.add(txtDelay, BorderLayout.EAST);
 
         lblKConnections = new JLabel(K_CONNECTIONS);
+        lblKConnections.setFont(new Font("Normal", Font.BOLD, 18));
         txtKConnections = new JTextField();
         txtKConnections.setColumns(20);
+        txtKConnections.setFont(new Font("Normal", Font.BOLD, 18));
         panelKConnections = new JPanel();
         panelKConnections.setLayout(new BorderLayout());
         panelKConnections.add(lblKConnections, BorderLayout.WEST);
         panelKConnections.add(txtKConnections, BorderLayout.EAST);
 
         lblNAvailableProcesses = new JLabel(N_AVAILABLE_PROCESSES);
+        lblNAvailableProcesses.setFont(new Font("Normal", Font.BOLD, 18));
         txtNAvailableProcesses = new JTextField();
         txtNAvailableProcesses.setColumns(20);
+        txtNAvailableProcesses.setFont(new Font("Normal", Font.BOLD, 18));
         panelNAvailableProcesses = new JPanel();
         panelNAvailableProcesses.setLayout(new BorderLayout());
         panelNAvailableProcesses.add(lblNAvailableProcesses, BorderLayout.WEST);
         panelNAvailableProcesses.add(txtNAvailableProcesses, BorderLayout.EAST);
 
         lblPAvailableProcesses = new JLabel(P_AVAILABLE_PROCESSES);
+        lblPAvailableProcesses.setFont(new Font("Normal", Font.BOLD, 18));
         txtPAvailableProcesses = new JTextField();
         txtPAvailableProcesses.setColumns(20);
+        txtPAvailableProcesses.setFont(new Font("Normal", Font.BOLD, 18));
         panelPAvailableProcesses = new JPanel();
         panelPAvailableProcesses.setLayout(new BorderLayout());
         panelPAvailableProcesses.add(lblPAvailableProcesses, BorderLayout.WEST);
         panelPAvailableProcesses.add(txtPAvailableProcesses, BorderLayout.EAST);
 
         lblMAvailableProcesses = new JLabel(M_AVAILABLE_PROCESSES);
+        lblMAvailableProcesses.setFont(new Font("Normal", Font.BOLD, 18));
         txtMAvailableProcesses = new JTextField();
         txtMAvailableProcesses.setColumns(20);
+        txtMAvailableProcesses.setFont(new Font("Normal", Font.BOLD, 18));
         panelMAvailableProcesses = new JPanel();
         panelMAvailableProcesses.setLayout(new BorderLayout());
         panelMAvailableProcesses.add(lblMAvailableProcesses, BorderLayout.WEST);
         panelMAvailableProcesses.add(txtMAvailableProcesses, BorderLayout.EAST);
 
         lblTimeout = new JLabel(T_TIMEOUT);
+        lblTimeout.setFont(new Font("Normal", Font.BOLD, 18));
         txtTimeout = new JTextField();
         txtTimeout.setColumns(20);
+        txtTimeout.setFont(new Font("Normal", Font.BOLD, 18));
         panelTimeout = new JPanel();
         panelTimeout.setLayout(new BorderLayout());
         panelTimeout.add(lblTimeout, BorderLayout.WEST);
         panelTimeout.add(txtTimeout, BorderLayout.EAST);
 
         chkFortuna = new JCheckBox(O_FORTUNA);
+        chkFortuna.setFont(new Font("Normal", Font.BOLD, 18));
         panelFortuna = new JPanel();
         panelFortuna.add(chkFortuna);
 
@@ -166,32 +183,140 @@ public class GUI extends JFrame{
         btnStart.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                displayData();
+                displayLiveStatistics();
             }
         });
 
-        super.add(panelNumberOfSimulations);
-        super.add(panelMaxTimePerSimulation);
-        super.add(panelSlowMode);
-        super.add(panelDelay);
-        super.add(panelKConnections);
-        super.add(panelNAvailableProcesses);
-        super.add(panelPAvailableProcesses);
-        super.add(panelMAvailableProcesses);
-        super.add(panelTimeout);
-        super.add(panelFortuna);
-        super.add(panelStart);
-        super.setSize(570, 600);
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new GridLayout(0, 1, 0, 20));
+
+        mainPanel.add(panelNumberOfSimulations);
+        mainPanel.add(panelMaxTimePerSimulation);
+        mainPanel.add(panelSlowMode);
+        mainPanel.add(panelDelay);
+        mainPanel.add(panelKConnections);
+        mainPanel.add(panelNAvailableProcesses);
+        mainPanel.add(panelPAvailableProcesses);
+        mainPanel.add(panelMAvailableProcesses);
+        mainPanel.add(panelTimeout);
+        mainPanel.add(panelFortuna);
+        mainPanel.add(panelStart);
+        Border padding = BorderFactory.createEmptyBorder(15, 15, 15, 15);
+        mainPanel.setBorder(padding);
+
+
+        super.add(mainPanel);
+        super.setSize(900, 900);
         super.setResizable(false);
         super.setVisible(true);
     }
 
-    public void displayData(){
-        dataDisplay = new JTextArea();
+    private void changeLayout(JComponent component){
+        Border padding = BorderFactory.createEmptyBorder(15, 15, 15, 15);
+        component.setBorder(padding);
         super.getContentPane().removeAll();
-        super.add(dataDisplay);
+        super.add(component);
         super.revalidate();
         super.repaint();
+    }
+
+    private void displayLiveStatistics(){
+        JTextArea txtArea = new JTextArea();
+        txtArea.setFont(new Font("Normal", Font.BOLD, 20));
+        txtArea.setEditable(false);
+        JPanel panel = new JPanel();
+        panel.setLayout(new BoxLayout(panel, BoxLayout.PAGE_AXIS));
+        JButton next = new JButton("Next");
+        next.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                displayFinalSimulationResult(0);
+            }
+        });
+        JScrollPane jsp = new JScrollPane(txtArea, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        panel.add(jsp);
+        panel.add(next);
+        changeLayout(panel);
+    }
+
+    public void displayFinalSimulationResult(int simulationNumber){
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+
+        JPanel panelSimulation = new JPanel();
+        panelSimulation.setLayout(new FlowLayout());
+        JLabel lblSimulation = new JLabel("Simulation number: ");
+        lblSimulation.setFont(new Font("Normal", Font.BOLD, 20));
+        JTextField txtSimulationNumber = new JTextField("" + simulationNumber);
+        txtSimulationNumber.setFont(new Font("Normal", Font.BOLD, 20));
+        txtSimulationNumber.setEditable(false);
+        panelSimulation.add(lblSimulation);
+        panelSimulation.add(txtSimulationNumber);
+
+        JPanel panelConnectionLife = new JPanel(new FlowLayout());
+        JLabel lblConnectionLife = new JLabel("Average connection lifetime: ");
+        lblConnectionLife.setFont(new Font("Normal", Font.BOLD, 20));
+        JTextField txtConnectionLifetime = new JTextField();
+        txtConnectionLifetime.setFont(new Font("Normal", Font.BOLD, 20));
+        txtConnectionLifetime.setEditable(false);
+        txtConnectionLifetime.setText("0.1");
+        panelConnectionLife.add(lblConnectionLife);
+        panelConnectionLife.add(txtConnectionLifetime);
+
+        JPanel panelRejectedConnections = new JPanel();
+        panelRejectedConnections.setLayout(new FlowLayout());
+        JLabel lblRejectedConnections = new JLabel("Rejected connections: ");
+        lblRejectedConnections.setFont(new Font("Normal", Font.BOLD, 20));
+        JTextField txtRejectedConnections = new JTextField();
+        txtRejectedConnections.setFont(new Font("Normal", Font.BOLD, 20));
+        txtRejectedConnections.setEditable(false);
+        txtRejectedConnections.setText("54");
+        panelRejectedConnections.add(lblRejectedConnections);
+        panelRejectedConnections.add(txtRejectedConnections);
+
+        JPanel panelAveragePerModuleTitle = new JPanel();
+        JLabel lblAveragePerModule = new JLabel("Average time per module (per type of query): ");
+        lblAveragePerModule.setFont(new Font("Normal", Font.BOLD, 20));
+        panelAveragePerModuleTitle.add(lblAveragePerModule);
+
+        JPanel panelAveragePerModule = new JPanel();
+        panelAveragePerModule.setLayout(new BoxLayout(panelAveragePerModule, BoxLayout.PAGE_AXIS));
+        panelAveragePerModule.setLayout(new GridLayout(3, 3, 5, 5));
+        JTextArea txtAverageInModule1 = new JTextArea("Client Connection Module (without a solved query): \nDDL: \t " + "\nJoin: \t" + "\nSelect: \t" + "\nUpdate: \t" + "\nTotal idle time: \t" + "\nTotal average time in queue: \t"); //TODO pasar cada promedio
+        JTextArea txtAverageInModule2 = new JTextArea("Query Processing Module: \nDDL: \t " + "\nJoin: \t" + "\nSelect: \t" + "\nUpdate: \t" + "\nTotal idle time: \t" + "\nTotal average time in queue: \t");
+        JTextArea txtAverageInModule3 = new JTextArea("Process Manager Module: \nDDL: \t" + "\nJoin: \t" + "\nSelect: \t" + "\nUpdate: \t" + "\nTotal idle time: \t" + "\nTotal average time in queue: \t");
+        JTextArea txtAverageInModule4 = new JTextArea("Transaction and Data Access Module: \nDDL: \t" + "\nJoin: \t" + "\nSelect: \t" + "\nUpdate: \t" + "\nTotal idle time: \t" + "\nTotal average time in queue: \t");
+        JTextArea txtAverageInModule5 = new JTextArea("Execution Module: \nDDL: \t" + "\nJoin: \t" + "\nSelect: \t" + "\nUpdate: \t" + "\nTotal idle time: \t" + "\nTotal average time in queue: \t");
+        JTextArea txtAverageInModule6 = new JTextArea("Client Connection Module (with a solved query): \nDDL: \t" + "\nJoin: \t" + "\nSelect: \t" + "\nUpdate: \t" + "\nTotal idle time: \t" + "\nTotal average time in queue: \t");
+
+        txtAverageInModule1.setFont(new Font("Normal", Font.BOLD, 15));
+        txtAverageInModule2.setFont(new Font("Normal", Font.BOLD, 15));
+        txtAverageInModule3.setFont(new Font("Normal", Font.BOLD, 15));
+        txtAverageInModule4.setFont(new Font("Normal", Font.BOLD, 15));
+        txtAverageInModule5.setFont(new Font("Normal", Font.BOLD, 15));
+        txtAverageInModule6.setFont(new Font("Normal", Font.BOLD, 15));
+
+        txtAverageInModule1.setEditable(false);
+        txtAverageInModule2.setEditable(false);
+        txtAverageInModule3.setEditable(false);
+        txtAverageInModule4.setEditable(false);
+        txtAverageInModule5.setEditable(false);
+        txtAverageInModule6.setEditable(false);
+
+        panelAveragePerModule.add(txtAverageInModule1);
+        panelAveragePerModule.add(txtAverageInModule2);
+        panelAveragePerModule.add(txtAverageInModule3);
+        panelAveragePerModule.add(txtAverageInModule4);
+        panelAveragePerModule.add(txtAverageInModule5);
+        panelAveragePerModule.add(txtAverageInModule6);
+
+
+        mainPanel.add(panelSimulation);
+        mainPanel.add(panelConnectionLife);
+        mainPanel.add(panelRejectedConnections);
+        mainPanel.add(panelAveragePerModuleTitle);
+        mainPanel.add(panelAveragePerModule);
+        changeLayout(mainPanel);
     }
 
     public static void main(String... args){
