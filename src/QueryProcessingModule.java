7af8c3cb240a96ae61767be1e17b1d55305ab30d
Brayan <bjgd95@gmail.com>
2017-02-18 10:56:31 -0600

    Implementación de evento Kill Agregué hashTable para mapear ud id con el evento de su kill Agregué variable booleana para condicionar el paso a siguiente módulo

--- src/QueryProcessingModule.java
+++ src/QueryProcessingModule.java
@@ -79,6 +79,7 @@ public class QueryProcessingModule extends Module {
 
     @Override
     public void processKill(Query query) {
+        
         //Si está en cola, sacarlo
         if(query.getIsInQueue()){
             queue.remove(query);
